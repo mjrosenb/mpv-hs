47b27705b3c025b0bedd1e63df1defd766050e57
marty rosenberg <marty.rosenberg@gmail.com>
2015-06-01 00:46:49 -0400

    Update the example to use the new high level bindings.

--- src/simple.hs
+++ src/simple.hs
@@ -1,37 +1,25 @@
 {-# LANGUAGE RecursiveDo #-}
 import MPV
 import MPV.Event
+import MPV.Raw.Event
 import Foreign.C
 import Foreign.C.String
 import Foreign.Marshal.Array
 import Foreign.Ptr
 import System.Environment
 import Foreign.Storable
-recCize' f all [] =  f all
-recCize' f a (h:t) = withCString h (\cur -> recCize' f (cur:a) t)
-recCize :: ([CString] -> IO a) -> [String] -> IO a
-recCize f l =
-     recCize' (f . reverse) [] l
-
-callWithArray :: (Ptr CString -> IO a) -> [String] -> IO a
-callWithArray f argv =
-    let innerCall cstrings = withArray0 nullPtr cstrings f
-    in recCize innerCall argv
 main = do
-  ctx <- mpv_create
-  withCString "input-default-bindings" (\ name ->
-   withCString "yes" (\val -> mpv_set_option_string ctx name val))
-  withCString "input-vo-keyboard" (\ name ->
-   withCString "yes" (\val -> mpv_set_option_string ctx name val))
-  mpv_initialize ctx
+  ctx <- MPV.create
+  MPV.setOptionString ctx "input-default-bindings" "yes"
+  MPV.setOptionString ctx "input-vo-keyboard" "yes"
+  MPV.initialize ctx
   args <- getArgs
-  callWithArray (mpv_command ctx) ["loadfile", args!!0]
+  MPV.command ctx ["loadfile", args!!0]
   let loop = do
-         MPVEvent event <- mpv_wait_event ctx 10000
-         x <- peek event
-         print x
-         case eventID x of
+         event <- MPV.waitEvent ctx 10000
+         print event
+         case eventID event of
            Shutdown -> return ()
            _ -> loop
   loop
-  mpv_terminate_destroy ctx
+  MPV.terminateDestroy ctx
